fd96170fd15ccbe0b42463fe8d4ac78f511d10c7
Corey Oordt <coordt@washingtontimes.com>
2010-08-16 10:55:17 -0400

    Put in some testing code to test the new overrides

--- example/testtags/admin.py
+++ example/testtags/admin.py
@@ -4,5 +4,15 @@ from models import TestName
 class TestNameAdmin(admin.ModelAdmin):
     model = TestName
     alphabet_filter = 'sorted_name'
+    
+    ## Testing a custom Default Alphabet
+    #DEFAULT_ALPHABET = 'ABC'
+    
+    ## Testing a blank alphabet-- only shows the characters in the database
+    #DEFAULT_ALPHABET = ''
+    
+    ## Testing a callable
+    def DEFAULT_ALPHABET(self):
+        return "I D K W"
 
 admin.site.register(TestName, TestNameAdmin)
