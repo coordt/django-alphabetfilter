151c5907e3301344f1f73dbec1f9baa64e8e694e
Corey Oordt <coordt@washingtontimes.com>
2010-08-20 09:59:36 -0400

    Added a new registry so users can decorate a 3rd party app, like a Django contrib app, with an alpha filter

---
+++ alphafilter/admin.py
@@ -0,0 +1,18 @@
+from django.db.models import get_model
+from django.contrib import admin
+from django.conf import settings
+
+MODEL_REGISTRY = getattr(settings, 'ALPHAFILTER_ADMIN_FIELDS', ())
+FIELDS = {}
+
+for key, val in MODEL_REGISTRY.items():
+    if isinstance(key, basestring):
+        FIELDS[get_model(*key.split('.'))] = val
+
+
+for model,modeladmin in admin.site._registry.items():
+    if model in FIELDS:
+        admin.site.unregister(model)
+        admin.site.register(model, type('newadmin', (modeladmin.__class__,), {
+            'alphabet_filter': FIELDS[model],
+        }))
