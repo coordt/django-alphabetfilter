a687ea5030307cb73dd3ab9d3c0f59fc494f5ce2
Ales Zabala Alava (Shagi) <shagi@gisa-elkartea.org>
2012-02-09 14:36:32 +0100

    provide alphafilter enabled generic view: AlphafilterListView

--- alphafilter/views.py
+++ alphafilter/views.py
@@ -3,6 +3,7 @@ A generic view for filtering querysets via alphafilter
 """
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.generic import ListView
 
 def alphafilter(request, queryset, template):
     """
@@ -20,3 +21,15 @@ def alphafilter(request, queryset, template):
         {'objects': queryset.filter(**qs_filter)}, 
         context_instance=RequestContext(request)
     )
+
+class AlphafilterListView(ListView):
+    def get_queryset(self):
+        queryset = super(AlphafilterListView, self).get_queryset()
+        
+        qs_filter = {}
+        for key in self.request.GET.keys():
+            if '__istartswith' in key:
+                qs_filter[str(key)] = self.request.GET[key]
+                break
+        
+        return queryset.filter(**qs_filter)
