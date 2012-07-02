388c3ed5c089cf1cd6f2f75677c71f18ee1d1c42
Corey Oordt <coreyoordt@gmail.com>
2012-07-02 07:42:59 -0500

    Version bump to 0.5.5

--- alphafilter/__init__.py
+++ alphafilter/__init__.py
@@ -1,12 +1,12 @@
 """
-Django-AlphaFilter provides an admin widget for alphabetical filtering that 
+Django-AlphaFilter provides an admin widget for alphabetical filtering that
 works like date_hierarchy and an template tag for use elsewhere.
 """
 
 __version_info__ = {
     'major': 0,
     'minor': 5,
-    'micro': 4,
+    'micro': 5,
     'releaselevel': 'final',
     'serial': 0
 }
